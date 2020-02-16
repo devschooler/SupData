11de3600e4ab7bf041aaf5b78af245ba105eef96
Wesley <wesly972@hotmail.com>
2020-02-16 23:55:08 +0100

    setup first spark calcul and percetage

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
+from __future__ import division
+
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-
 working_directory = 'jars/*'
 
 spark = SparkSession \
@@ -13,6 +14,25 @@ spark = SparkSession \
     .config('spark.driver.extraClassPath', working_directory) \
     .getOrCreate()
 
+
 df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").load()
-lama = df.count()
-print(lama)
+total_students = df.count()
+#print(lama)
+
+pipeline = "{'$match' : {'name': 'LAMA'}}"
+df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").option("pipeline",pipeline).load()
+#print(df)
+#counter = df.collect()
+#print(counter)
+
+#what is the impact of student fair on recruitments
+
+pipeline = "{'$match' : {'hired after student fair': true}}"
+df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").option("pipeline",pipeline).load()
+number_hired_after_fair = df.count()
+
+data =  (number_hired_after_fair / total_students ) * 100
+data_str = str(data)
+fair_percent = "%.1f" % data
+
+print(number_hired_after_fair , 'students are hired after student fair, representing ', fair_percent,'percent of the students')
