39fc9e0458eb35fb7cc249501bd6d125ebcb3ccf
Wesley <wesly972@hotmail.com>
2020-02-19 00:38:47 +0100

    calcul moyenne paris OK

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from __future__ import division
+import pymongo
+from pymongo import MongoClient
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
@@ -14,18 +16,13 @@ spark = SparkSession \
     .config('spark.driver.extraClassPath', working_directory) \
     .getOrCreate()
 
-
+#total students
 df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").load()
 total_students = df.count()
-#print(lama)
 
-pipeline = "{'$match' : {'name': 'LAMA'}}"
-df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").option("pipeline",pipeline).load()
-#print(df)
-#counter = df.collect()
-#print(counter)
 
-#what is the impact of student fair on recruitments
+
+#5 what is the impact of student fair on recruitments
 
 pipeline = "{'$match' : {'hired after student fair': true}}"
 df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").option("pipeline",pipeline).load()
@@ -36,3 +33,36 @@ data_str = str(data)
 fair_percent = "%.1f" % data
 
 print(number_hired_after_fair , 'students are hired after student fair, representing ', fair_percent,'percent of the students')
+
+#1 succes depending on region
+
+cluster = pymongo.MongoClient("mongodb://localhost:27017/")
+
+#calcul total number of paris student 
+pipeline = "{'$match' : {'campus': 'Paris'}}"
+df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").option("pipeline",pipeline).load()
+parisStudentNumber = df.count()
+print(parisStudentNumber)
+
+#Calcul moyenne etudiants paris 
+
+pipeline = [{'$match' : {'campus': 'Paris'}},
+            {'$group':{ '_id': '$campus', 'total': { '$sum': '$overall average' }}}, 
+            ]
+
+          #  {'$group':{'_id':{'myid':'$myid'}, 'record':{'$first':'$$ROOT'}}}, 
+
+df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://127.0.0.1/SupinfoDB.students").option("pipeline",pipeline).load()
+
+total_notes_paris = df.collect()
+print(total_notes_paris)
+stringed_notes = str(total_notes_paris[0])
+total_str_notes = stringed_notes[24:28]
+total_notes = int(total_str_notes)
+print(type(total_notes))
+
+moyenne_paris = total_notes / parisStudentNumber 
+print( 'la moyenne de paris est ' , moyenne_paris)
+
+
+
